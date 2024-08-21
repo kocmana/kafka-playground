8cf7c9a066919ac36037919da3bcd3ebf1ce552f
Andreas Kocman <andreas.kocman@gmail.com>
2024-08-21 12:48:16 +0200

    Batch handling experimentation

--- customer-consumer/src/main/java/at/kocmana/kafkaplayground/customerconsumer/configuration/KafkaConfiguration.java
+++ customer-consumer/src/main/java/at/kocmana/kafkaplayground/customerconsumer/configuration/KafkaConfiguration.java
@@ -1,9 +1,12 @@
 package at.kocmana.kafkaplayground.customerconsumer.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @Configuration
 @EnableKafka
 public class KafkaConfiguration {
+
+
 }
--- customer-consumer/src/main/java/at/kocmana/kafkaplayground/customerconsumer/customer/CustomerController.java
+++ customer-consumer/src/main/java/at/kocmana/kafkaplayground/customerconsumer/customer/CustomerController.java
@@ -2,6 +2,7 @@ package at.kocmana.kafkaplayground.customerconsumer.customer;
 
 import at.kocmana.kafkaplayground.Customer;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 
@@ -9,9 +10,19 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class CustomerController {
 
+  /*@KafkaListener(topics = "customers")
+  void retrieveCustomer(@Payload Customer customer, @Headers Map<String, Object> header) {
+    var offset = header.get(KafkaHeaders.OFFSET);
+    log.info("Customer with offset {} retrieved: {} ", offset, customer);
+  }
+  */
+
   @KafkaListener(topics = "customers")
-  void retrieveCustomer(Customer customer) {
-    log.info("Customer retrieved: {}", customer);
+  void retrieveCustomersBatched(ConsumerRecords<String, Customer> records) {
+    for (var record : records) {
+      var value = record.value();
+      log.info("Received customer record: {}", value);
+    }
   }
 
 }
